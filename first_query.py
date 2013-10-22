a4d32f5eb2e75111b3dfa9bc2498b6b6e2582067
Géraud Le Falher <daureg@gmail.com>
2013-10-22 18:40:38 +0300

    look at user “productivity”

--- first_query.py
+++ first_query.py
@@ -12,11 +12,7 @@ def last_query_time(db):
     return db.system.profile.find().sort('ts', -1).limit(1)[0]['millis']
 
 
-if __name__ == '__main__':
-    client = pymongo.MongoClient('localhost', 27017)
-    db = client['flickr']
-    photos = db['photos']
-    db.set_profiling_level(pymongo.ALL)
+def tag_count(db):
     start = clock()
     tags = photos.aggregate([
         {"$unwind": "$tags"},
@@ -29,34 +25,38 @@ if __name__ == '__main__':
     name = map(itemgetter('_id'), tags['result'])
     count = map(itemgetter('count'), tags['result'])
 
-    import sys
     nonascii = 0
     for i, n in enumerate(name):
         try:
-            _ = str(n)
+            str(n)
         except UnicodeEncodeError:
             # print(n)
             nonascii += count[i]
 
     print(nonascii, sum(count))
     outplot('tags_count.dat', ['tag', 'count'], name, count)
-    # print(db.profiling_info())
-    # mapper = Code("""
-    #               function () {
-    #                   this.tags.forEach(function(z) {
-    #                       emit(z, 1);
-    #                  });
-    #              }""")
-    # reducer = Code("""
-    #                function (key, values) {
-    #                    var total = 0;
-    #                    for (var i = 0; i < values.length; i++) {
-    #                        total += values[i];
-    #                    }
-    #                    return total;
-    #                }
-    #                """)
-    # res = photos.map_reduce(mapper, reducer, "myresults", full_response=True)
-    # t = res['timeMillis']
-    # tag = db.myresults.find().sort('value', -1)[0]
-    # print('mapRed in {}ms ({})'.format(t, tag))
+
+
+def user_loc(db):
+    start = clock()
+    # users = photos.aggregate([
+    #     {'$project': {'_id': 0, 'user': '$uid'}},
+    #     {'$group': {'_id': '$user', 'photos': {'$sum': 1}}},
+    #     {'$sort': SON([('photos', -1)])}
+    # ])
+    users = photos.aggregate([
+        {'$project': {'_id': 0, 'loc.coordinates': 1, 'user': '$uid'}},
+        {'$group': {'_id': '$user', 'pos': {'$push': '$loc.coordinates'}}},
+        {'$limit': 5}
+    ])
+    t = 1000*(clock() - start)
+    print('aggregate in {:.3f}ms ({})'.format(t, users['result'][0]))
+    return users
+
+if __name__ == '__main__':
+    client = pymongo.MongoClient('localhost', 27017)
+    db = client['flickr']
+    photos = db['photos']
+    db.set_profiling_level(pymongo.ALL)
+    # u, c = user_count(photos)
+    u = user_loc(photos)

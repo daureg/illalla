cafc811a894cb7525e5cc4160ff7122090675405
Géraud Le Falher <daureg@gmail.com>
2013-10-28 09:58:52 +0200

    plot frequencies with a colormap

--- more_query.py
+++ more_query.py
@@ -2,9 +2,11 @@
 # vim: set fileencoding=utf-8
 from outplot import outplot
 from timeit import default_timer as clock
+import json
+import colour
 import datetime
 import pymongo
-from shapely.geometry import Point, mapping
+from shapely.geometry import Point, shape, mapping
 from math import floor
 # from sys import float_info
 # EPSILON = 1000*float_info.epsilon
@@ -13,16 +15,10 @@ import numpy as np
 from scipy.spatial.distance import pdist, squareform
 from operator import itemgetter
 
+CSS = '#{} {{fill: {}; opacity: 0.5; stroke-width: .3px;}}'
 KARTO_CONFIG = {'bounds': {'data': [-122.4, 37.768, -122.38, 37.778],
                            'mode': 'bbox'},
-                'export': {"round": 1},
-                'layers': {'photos': {'src': 'baseball_3.shp'},
-                           'coastline': {'src': 'bayarea_county2000.shp'},
-                           'lakes': {'src': 'v1_lake.shp',
-                                     'substract-from': 'coastline'},
-                           'park': {'src': 'v1_park.shp'},
-                           'road': {'src': 'v1_road.shp'},
-                           'urban': {'src': 'v1_urban.shp'}},
+                'layers': {},
                 'proj': {'id': 'laea', 'lat0': 37.78, 'lon0': -122.4}}
 
 
@@ -34,7 +30,7 @@ def total_seconds(td):
     return td.seconds + td.days * 24 * 3600
 
 
-def bbox_to_polygon(bbox):
+def bbox_to_polygon(bbox, latitude_first=True):
     """Return a 5 points GeoJSON polygon based on the bottom left and upper
     right coordinates of bbox [lat_bl, long_bl, lat_ur, long_ur]
     (5 because the polygon needs to be closed, see:
@@ -43,7 +39,10 @@ def bbox_to_polygon(bbox):
     {'type': 'Polygon', 'coordinates': [[[-122, 37], [-120, 37], [-120, 35], [-122, 35], [-122, 37]]]}
     """
     assert(len(bbox) == 4)
-    lat_bl, long_bl, lat_ur, long_ur = bbox
+    if latitude_first:
+        lat_bl, long_bl, lat_ur, long_ur = bbox
+    else:
+        long_bl, lat_bl, long_ur, lat_ur = bbox
     r = {}
     r['type'] = 'Polygon'
     r['coordinates'] = [[[long_bl, lat_bl], [long_ur, lat_bl],
@@ -112,31 +111,30 @@ def tag_over_time(collection, tag, bbox, start, interval, user_status=None):
 
 
 def k_split_bbox(bbox, k=2, offset=0):
-    long_step = (bbox[2] - bbox[0])/k
-    lat_step = (bbox[3] - bbox[1])/k
+    long_step = (bbox[3] - bbox[1])/k
+    lat_step = (bbox[2] - bbox[0])/k
     assert(offset <= min(long_step, lat_step))
     hoffset = offset
     voffset = 0
-    x = bbox[0]
-    y = bbox[1]
+    x = bbox[1]
+    y = bbox[0]
     region = []
-    while y + voffset < bbox[3] - 0.5*lat_step:
-        while x + hoffset < bbox[2] - 0.5*long_step:
+    while y + voffset < bbox[2] - 0.5*lat_step:
+        while x + hoffset < bbox[3] - 0.5*long_step:
             region.append([x + hoffset, y + voffset,
                            x + hoffset + long_step, y + voffset + lat_step])
             x += long_step
 
         y += lat_step
-        x = bbox[0]
+        x = bbox[1]
 
     def coord2region(coords):
         longitude, latitude = coords
         # TODO handle correctly edge case
-        x = longitude - hoffset - bbox[0] - 1e-8
-        y = latitude - voffset - bbox[1] - 1e-8
+        x = longitude - hoffset - bbox[1] - 1e-8
+        y = latitude - voffset - bbox[0] - 1e-8
         if x < 0 or y < 0:
             return -1
-        print(x, y, x/long_step, y/lat_step)
         r = k*int(floor(y/lat_step)) + int(floor(x/long_step))
         if r > k*k - 1:
             return - 1
@@ -145,25 +143,55 @@ def k_split_bbox(bbox, k=2, offset=0):
     return region, coord2region
 
 
-def compute_frequency(collection, tag, bbox, start, end, k=3, uploaded=False):
+def compute_frequency(collection, tag, bbox, start, end, k=3,
+                      exclude_zero=True, uploaded=False):
+    """split bbox in k^2 rectangles and compute the frequency of tag in each of
+    them. Return a list of list of Polygon, grouped by similar frequency
+    (potentialy omiting the zero one for clarity)."""
+    # global KARTO_CONFIG
     coords = tag_location(collection, tag, bbox, start, end, uploaded)
-    print(bbox)
     r, f = k_split_bbox(bbox, k)
-    print(r)
-    print(coords[:10])
-    print(f(coords[0]))
+    # count[0] is for potential points that do not fall in any region (it must
+    # only happens because of rounding inprecision)
     count = (len(r)+1)*[0, ]
     for loc in coords:
         count[f(loc)+1] += 1
 
     N = len(coords)
-    print(N, count[0])
-    freq = np.array(count[1:])
-    return r, freq/(1.0*N)
-# TODO call tag_location, go over the list, accumulate by region, divide to get
-# frequency, group region of similar frequency into a collection of Polygon,
-# make config.json a python dictionnary, inline style each frequency with a
-# different shade of red
+    freq = np.array(count[1:])/(1.0*N)
+    nb_inter = 3
+    interval_size = (np.max(freq) - np.min(freq))/nb_inter
+    bucket = []
+    for i in range(nb_inter):
+        bucket.append([])
+    for i, v in enumerate(freq):
+        if not (exclude_zero and v < 1e-8):
+            poly = {'geometry': mapping(shape(bbox_to_polygon(r[i], False))),
+                    'properties': {}}
+            index = (min(nb_inter - 1, int(floor(v / interval_size))))
+            bucket[index].append(poly)
+    return bucket
+
+
+def plot_polygons(bucket, tag, nb_inter=3):
+    schema = {'geometry': 'Polygon', 'properties': {}}
+    low_color = colour.Color(rgb=(255/255.0, 255/255.0, 204/255.0))
+    high_color = colour.Color(rgb=(177/255.0, 0, 38/255.0))
+    colormap = list(low_color.range_to(high_color, nb_inter))
+    style = []
+    for i in range(nb_inter):
+        if len(bucket[i]) > 0:
+            name = '{}_freq_{}'.format(tag, i+1)
+            KARTO_CONFIG['layers'][name] = {'src': name+'.shp'}
+            style.append(CSS.format(name, colormap[i].hex))
+            with fiona.collection(name+'.shp', "w",
+                                  "ESRI Shapefile", schema) as f:
+                f.writerecords(bucket[i])
+
+    with open('photos.json', 'w') as f:
+        json.dump(KARTO_CONFIG, f)
+    with open('photos.css', 'w') as f:
+        f.write('\n'.join(style))
 
 
 def simple_metrics(collection, tag, bbox, start, end):
@@ -215,25 +243,29 @@ def classify_users(db):
 
 
 if __name__ == '__main__':
+    start = clock()
     client = pymongo.MongoClient('localhost', 27017)
     db = client['flickr']
     photos = db['photos']
     SF_BBOX = [37.7123, -122.531, 37.84, -122.35]
+    # r, f = k_split_bbox(SF_BBOX, 3)
     # import doctest
     # doctest.testmod()
-    # r, f = compute_frequency(photos, 'baseball',
-    #                          [37.768, -122.4, 37.778, -122.38],
-    #                          datetime.datetime(2008, 1, 1),
-    #                          datetime.datetime(2014, 1, 1))
-    start = clock()
+    t = 1000*(clock() - start)
+    print('connect in {:.3f}ms'.format(t))
+    b = compute_frequency(photos, 'baseball',
+                          [37.768, -122.4, 37.778, -122.38],
+                          datetime.datetime(2008, 1, 1),
+                          datetime.datetime(2014, 1, 1))
+    plot_polygons(b, 'baseball')
     # classify_users(db, photos)
     # u = get_user_status(db['users'])
     # tag_over_time(photos, 'local', None,
     #               datetime.datetime(2005, 1, 1),
     #               datetime.timedelta(days=3218), u)
-    simple_metrics(photos, 'baseball',
-                   [37.768, -122.4, 37.778, -122.38],
-                   datetime.datetime(2008, 1, 1),
-                   datetime.datetime(2014, 1, 1))
+    # simple_metrics(photos, 'baseball',
+    #                [37.768, -122.4, 37.778, -122.38],
+    #                datetime.datetime(2008, 1, 1),
+    #                datetime.datetime(2014, 1, 1))
     t = 1000*(clock() - start)
     print('aggregate in {:.3f}ms'.format(t))

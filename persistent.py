fe49514279cb28e4c5b748f836fb1f2107e6e062
Géraud Le Falher <Graud Le Falher daureg@gmail.com>
2013-11-02 21:16:10 +0200

    use pickle and throw exception if file doesn't exist.

--- persistent.py
+++ persistent.py
@@ -1,17 +1,17 @@
 #! /usr/bin/python2
 # vim: set fileencoding=utf-8
+import pickle
 
 
 def save_var(filename, d):
-    import cPickle
-    with open(filename, 'w') as f:
-        pck = cPickle.Pickler(f)
-        pck.dump(d)
+    with open(filename, 'wb') as f:
+        pickle.dump(d, f, 2)
 
 
 def load_var(filename):
-    import cPickle
-    with open(filename) as f:
-        pck = cPickle.Unpickler(f)
-        d = pck.load()
+    try:
+        with open(filename, 'rb') as f:
+            d = pickle.load(f)
+    except IOError:
+        raise
     return d

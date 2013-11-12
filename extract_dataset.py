057feeeb00a18f54f19e8b2df4b6f65f990e1541
Géraud Le Falher <daureg@gmail.com>
2013-11-12 23:47:31 +0200

    get top 800 tags' photos in a matrix

--- extract_dataset.py
+++ extract_dataset.py
@@ -6,7 +6,11 @@ from timeit import default_timer as clock
 from bson.son import SON
 from more_query import FIRST_TIME, LAST_TIME, compute_entropy
 from collections import Counter
-from persistent import save_var
+from persistent import save_var, load_var
+import scipy.sparse
+import numpy as np
+import scipy.io as sio
+import calendar
 DAY_STEP = 4
 
 
@@ -38,14 +42,23 @@ def supported_tags(DB, photos_threshold=150, users_threshold=25, timespan=500):
             if (p['last'] - p['first']).total_seconds() > interval]
 
 
-def tag_time(DB, tag):
+def tag_time(DB, tag, fm=None):
     query = {}
-    field = {'taken': 1, '_id': 0}
+    field = {'taken': 1, '_id': 0, 'uid': 1, 'ntags': 1, 'loc': 1}
     query['hint'] = 'sf'
-    query['ntags'] = tag
+    if isinstance(tag, list):
+        query['ntags'] = {'$in': tag}
+    else:
+        query['ntags'] = tag
     query['taken'] = {'$gte': FIRST_TIME, '$lte': LAST_TIME}
     cursor = DB.photos.find(query, field)
-    return [(p['taken'].weekday(), p['taken'].hour/DAY_STEP) for p in cursor]
+
+    def format_photo(p):
+        if fm is None:
+            return (p['taken'].weekday(), p['taken'].hour/DAY_STEP)
+        return fm(p)
+
+    return [format_photo(p) for p in cursor]
 
 
 def period_entropy(DB, tag):
@@ -53,13 +66,32 @@ def period_entropy(DB, tag):
     return float(compute_entropy(Counter(times)))
 
 
+def get_data(DB):
+    entropies = load_var('Hsupported')
+    tags = sorted([k for k, v in entropies.items() if 2.5 <= v <= 3.01])
+    save_var('mat_tag', tags)
+    u = load_var('user_status')
+    user_index = {k: i for i, k in enumerate(u)}
+
+    def format_photo(p):
+        user = user_index[p['uid']]
+        loc = p['loc']['coordinates']
+        taken = [p['taken'].weekday(), p['taken'].hour,
+                 calendar.timegm(p['taken'].utctimetuple())]
+        indicator = [int(t in p['ntags']) for t in tags]
+        return [user] + loc + taken + indicator
+
+    photos_feature = np.mat(tag_time(DB, tags, format_photo))
+    sio.savemat('deep', {'A': scipy.sparse.csr_matrix(photos_feature)})
+
 if __name__ == '__main__':
     client = pymongo.MongoClient('localhost', 27017)
     DB = client['flickr']
     s = clock()
-    tags = supported_tags(DB)
-    save_var('supported', tags)
-    entropies = {t[0]: period_entropy(DB, t[0]) for t in tags}
-    save_var('Hsupported', entropies)
+    # tags = supported_tags(DB)
+    # save_var('supported', tags)
+    # entropies = {t[0]: period_entropy(DB, t[0]) for t in tags}
+    # save_var('Hsupported', entropies)
+    get_data(DB)
     t = clock()
     print(t-s)

6b03f819c7bdcb467fb7f6f773aacbb41061f01c
Géraud Le Falher <daureg@gmail.com>
2014-02-17 20:39:54 +0200

    insert checkin into mongoDB

--- read_foursquare.py
+++ read_foursquare.py
@@ -4,14 +4,16 @@
 specified cities.
 In the following, x refers to the latitude and y to the longitude"""
 import cities
+import pymongo
 from collections import namedtuple, defaultdict
 from datetime import datetime
 from numpy import median
 # from calendar import timegm
 
 Point = namedtuple('Point', ['x', 'y'])
+Location = namedtuple('Location', ['type', 'coordinates'])
 CheckIn = namedtuple('CheckIn',
-                     ['tid', 'lid', 'uid', 'city', 'lat', 'lon', 't'])
+                     ['tid', 'lid', 'uid', 'city', 'loc', 'time'])
 Node = namedtuple('Node', ['val', 'left', 'right'])
 
 
@@ -60,9 +62,37 @@ class Bbox():
         return '{}: {:.2f}, {:.2f}'.format(self.name, self.bottom.x,
                                            self.bottom.y)
 
+
+def convert_checkin_for_mongo(checkin):
+    suitable = checkin._asdict()
+    # because namedtuple cannot have field starting with underscore.
+    # TODO: Should I use namedtuple in the first place?
+    suitable['_id'] = suitable['tid']
+    del suitable['tid']
+    return suitable
+
+
+def save_to_mongo(documents, destination):
+    converted = [convert_checkin_for_mongo(p) for p in documents]
+    ids = [checkin['_id'] for checkin in converted]
+    try:
+        destination.insert(converted, continue_on_error=True)
+    except pymongo.errors.DuplicateKeyError:
+        # it's not my dataset so there is no much I can do
+        pass
+
+
 if __name__ == '__main__':
     # import doctest
     # doctest.testmod()
+
+    client = pymongo.MongoClient('localhost', 27017)
+    db = client['foursquare']
+    checkins = db['checkin']
+    checkins.ensure_index([('loc', pymongo.GEOSPHERE),
+                           ('lid', pymongo.ASCENDING),
+                           ('city', pymongo.ASCENDING),
+                           ('time', pymongo.ASCENDING)])
     import sys
     infile = 'medium' if len(sys.argv) < 2 else sys.argv[1]
     all_cities = cities.US + cities.EU
@@ -78,7 +108,7 @@ if __name__ == '__main__':
                 return city.name
         return None
 
-    res = []
+    seen = []
     with open(infile) as f:
         # UserID\tTweetID\tLatitude\tLongitude\tCreatedAt\tText\tPlaceID
         for line in f:
@@ -92,11 +122,19 @@ if __name__ == '__main__':
             city = find_town(lat, lon, tree)
             if city is not None:
                 stats[city] += 1
-                tid, lid, uid = int(tid), int(lid, 16), int(uid)
+                tid, uid = int(tid), int(uid)
                 t = datetime.strptime(t, '%Y-%m-%d %H:%M:%S')
-                # to have numerical values
+                # to have more numerical values (but lid should be a 64bit
+                # unsigned integer which seems to be quite complicated in
+                # mongo)
                 # t = timegm(t.utctimetuple())
                 # city = cities.INDEX[city]
-                res.append(CheckIn(tid, lid, uid, city, lat, lon, t))
+                seen.append(CheckIn(tid, lid, uid, city,
+                                    Location('Point', [lon, lat])._asdict(), t))
+                if len(seen) > 5000:
+                    save_to_mongo(seen, checkins)
+                    seen = []
+
+    save_to_mongo(seen, checkins)
     counts = sorted(stats.iteritems(), key=lambda x: x[1], reverse=True)
     print('\n'.join(['{}: {}'.format(city, count) for city, count in counts]))

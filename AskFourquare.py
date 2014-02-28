5114bbf115f7995f7de9521f6761aa37760f55a8
Géraud Le Falher <daureg@gmail.com>
2014-02-28 12:42:50 +0200

    Find similar venues within same city

--- AskFourquare.py
+++ AskFourquare.py
@@ -4,7 +4,13 @@ from datetime import datetime
 from read_foursquare import Location
 import foursquare
 import cities
-import pymongo
+import CommonMongo as cm
+import pycurl
+import cStringIO as cs
+from lxml import etree
+PARSER = etree.HTMLParser()
+# thanks Google Chrome (although this is rather fragile)
+XPATH_QUERY = '//*[@id="container"]/div/div[2]/div[2]/div[2]/ul'
 from collections import namedtuple
 from api_keys import FOURSQUARE_ID as CLIENT_ID
 from api_keys import FOURSQUARE_SECRET as CLIENT_SECRET
@@ -121,6 +127,9 @@ def get_list_of(field, obj):
 
 def gather_all_entities_id(checkins, entity='lid', city=None, limit=None,
                            chunk_size=foursquare.MAX_MULTI_REQUESTS):
+    """Return at most `limit` `entity` ids within `city` by group of
+    `chunk_size`."""
+    assert entity in ['lid', 'uid'], 'what are you looking for?'
     query = [
         {'$match': {'lid': {'$ne': None}}},
         {'$project': {'_id': 0, entity: 1}},
@@ -142,10 +151,43 @@ def gather_all_entities_id(checkins, entity='lid', city=None, limit=None,
             batch = [_id]
     yield batch
 
+
+def similar_venues(vid, venue_db=None, client=None):
+    """Return similar venues to `vid` as suggested by Foursquare, either by
+    scrapping webpage (at most 3 results) or making an API call using `client`
+    (at most 5 results)."""
+    assert venue_db or client, 'Need more information'
+    if client:
+        try:
+            answer = client.venues.similar(vid)['similarVenues']['items']
+            return [str(v['id']) for v in answer]
+        except foursquare.FoursquareException as e:
+            print(e)
+            return None
+    answer = venue_db.find_one({'_id': vid})
+    if not answer:
+        print(vid + ' is not a existing venue id')
+        return None
+
+    buf = cs.StringIO()
+    fetcher = pycurl.Curl()
+    fetcher.setopt(pycurl.URL, answer['canonicalUrl'])
+    fetcher.setopt(pycurl.WRITEFUNCTION, buf.write)
+    try:
+        fetcher.perform()
+    except pycurl.error as e:
+        print(str(e))
+        return None
+    page = buf.getvalue()
+    tree = etree.fromstring(page, PARSER)
+    similars = tree.xpath(XPATH_QUERY)
+    if len(similars) == 0:
+        return []
+    return [c.attrib['data-venueid'] for c in similars[0].iterchildren()]
+
 if __name__ == '__main__':
     client = foursquare.Foursquare(CLIENT_ID, CLIENT_SECRET)
-    mongo_client = pymongo.MongoClient('localhost', 27017)
-    db = mongo_client['foursquare']
+    db = cm.connect_to_db('foursquare')[0]
     checkins = db['checkin']
     # print(venue_profile(client, ''))
     # up = user_profile(client, 2355635)
@@ -153,6 +195,14 @@ if __name__ == '__main__':
     #         '40a55d80f964a52020f31ee3', '4b4ad9dff964c5200']
     # [client.venues(vid, multi=True) for vid in vids]
     # answers = list(client.multi())
-    r = gather_all_entities_id(checkins, city='helsinki', limit=50)
-    for b in r:
-        print(b)
+    # r = gather_all_entities_id(checkins, city='helsinki', limit=50)
+    # for b in r:
+    #     print(b)
+    svids = ['4c4787646c379521a121cfb5', '43222200f964a5209a271fe3',
+             '4b218c2ef964a520a83d24e3']
+    gold = [[], ['4bbd0fbb8ec3d13acea01b28', '451d2412f964a5208a3a1fe3'],
+            ['4d72a2a9ec07548190588cbf', '4a736a23f964a52062dc1fe3',
+             '4f2d3e99e4b056f83aecdc88', '4aa7b5e4f964a520064d20e3',
+             '4b2a2b72f964a520bca524e3']]
+    for query, response in zip(svids, gold):
+        print(similar_venues(query, client=client), response)

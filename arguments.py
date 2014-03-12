fa4b29efe04339db274d55a58ba176cd153d4047
Géraud Le Falher <daureg@gmail.com>
2014-03-12 17:19:40 +0200

    provide common arguments parsing with sanity check

---
+++ arguments.py
@@ -0,0 +1,55 @@
+#! /usr/bin/python2
+# vim: set fileencoding=utf-8
+"""Provide a standard argument parser."""
+import argparse
+
+
+def get_parser(desc=None):
+    """build a parser with host and port number"""
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("-p", "--port", help="MongoDB port number",
+                        type=lambda x: valid_number(x, 1023, 64536),
+                        default=27017)
+    parser.add_argument("--host", help="MongoDB host name",
+                        default="localhost")
+    return parser
+
+
+def valid_city(city):
+    """Ensure city argument is valid"""
+    import cities
+    city = cities.short_name(city)
+    if city in cities.SHORT_KEY:
+        return city
+    raise argparse.ArgumentTypeError('{} is not a known city'.format(city))
+
+
+def city_parser(desc=None):
+    """default parser plus city name"""
+    parser = get_parser(desc)
+    parser.add_argument("city", help="city name", type=valid_city)
+    return parser
+
+
+def valid_number(number, lower, upper, ntype=int):
+    """Ensure numeric argument is within bounds"""
+    try:
+        number = ntype.__call__(number)
+    except ValueError:
+        raise argparse.ArgumentTypeError('{} is not a number'.format(number))
+    if lower < number < upper:
+        return number
+    msg = '{} is not between {} and {}'.format(number, lower+1, upper-1)
+    raise argparse.ArgumentTypeError(msg)
+
+
+def tweets_parser():
+    """default parser plus run time duration"""
+    parser = get_parser('stream Twitter to find checkins')
+    parser.add_argument("duration", help="how many hours to run",
+                        type=lambda x: valid_number(x, 0, 24, float))
+    return parser
+
+if __name__ == '__main__':
+    ARGS = tweets_parser()
+    ARGS.parse_args()

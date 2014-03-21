94110be18cf3f793fcdde151be704b862f864551
Géraud Le Falher <daureg@gmail.com>
2014-03-21 09:39:10 +0200

    vectorize geo projection

--- LocalCartesian.py
+++ LocalCartesian.py
@@ -10,6 +10,10 @@ from geographiclib.geodesic import Geodesic
 EARTH = Geodesic.WGS84
 from geographiclib.geomath import Math as geomath
 from geographiclib.constants import Constants as geoconst
+_a = geoconst.WGS84_a
+_f = geoconst.WGS84_f
+_e2 = _f * (2 - _f)
+_e2m = 1 - _e2
 import math
 import numpy
 
@@ -56,6 +60,32 @@ def earth_forward(lat, lon, h):
     return numpy.array([X, Y, Z])
 
 
+def bforward(coords):
+    angles = to_unsafe_angles(coords)
+    n = _a/numpy.sqrt(1 - _e2 * angles[:, 0] * angles[:, 0])
+    Z = (_e2m * n + coords[:, 2]) * angles[:, 0]
+    X = (n + coords[:, 2]) * angles[:, 1]
+    Y = X * angles[:, 2]
+    X *= angles[:, 3]
+    return numpy.vstack([X, Y, Z]).T
+
+
+def to_unsafe_angles(coords):
+    """Return a n×4 array of (sphi, cphi, slam, clam) from a n×2 array
+    `coords` of (lat, lon), without any validity checks or correction."""
+    phi = numpy.radians(coords[:, 0])
+    lam = numpy.radians(coords[:, 1])
+    return numpy.vstack([numpy.sin(phi), numpy.cos(phi),
+                         numpy.sin(lam), numpy.cos(lam)]).T
+
+
+def to_unsafe_coords(angles):
+    """Reverse the action of to_unsafe_angles."""
+    lat = numpy.degrees(numpy.arcsin(angles[:, 0]))
+    aslon = numpy.arcsin(angles[:, 2])
+    lon = numpy.sign(aslon) * numpy.degrees(numpy.arccos(angles[:, 3]))
+    return numpy.vstack([lat, lon]).T
+
 def geocentric_rotation(sphi, cphi, slam, clam):
     """Geocentric::Rotation"""
     return numpy.matrix([
@@ -69,7 +99,10 @@ if __name__ == '__main__':
     # echo "60.15 24.91 0" | CartConvert -l 60.19415 24.92945 0
     ref = '-1080.3931874725 -4918.8217761851 -1.9863406491'.split()
     center = LocalCartesian(60.19415, 24.92945)
-    res = center.forward([60.15, 24.91])
-    print(res)
-    print(ref)
-    print(numpy.allclose(map(float, ref), res))
+    # res = center.forward([60.15, 24.91])
+    # print(res)
+    # print(ref)
+    # print(numpy.allclose(map(float, ref), res))
+    tdata = numpy.array([(60.15, 24.91, 10),
+                         (60.19415, 24.92945, 30),
+                         (59, 25, 0)])

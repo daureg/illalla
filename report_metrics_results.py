332b507090cd46d51646860822dd8199e74c3ac5
Géraud Le Falher <daureg@gmail.com>
2014-07-09 17:28:03 +0300

    make html_report runnable in Ipython

--- report_metrics_results.py
+++ report_metrics_results.py
@@ -15,41 +15,43 @@ import cities as c
 import pandas as pd
 from operator import itemgetter
 
-RES_SIZE = 5
-query_city = sys.argv[1]
-with open('static/cmp_{}.json'.format(query_city)) as gt:
-    results = json.load(gt)
-METRICS = sorted(set([str(_['metric'])
-                      for _ in results.values()[0].values()[0]]))
-with open('static/ground_truth.json') as gt:
-    regions = json.load(gt)
-DISTRICTS = sorted(regions.keys())
-CITIES = sorted(regions.items()[0][1]['gold'].keys())
-assert query_city in CITIES, ', '.join(CITIES)
-CITIES.remove(query_city)
-ALL_QUERIES = set(itertools.product(CITIES, DISTRICTS))
-NO_GOLD = []
-for d, g in regions.iteritems():
-    g = g['gold']
-    missing = set(CITIES).difference(set(g.keys()))
-    for city in missing:
-        NO_GOLD.append((city, d))
-print_region = lambda x: '  • '+str(x[0]).ljust(15)+str(x[1])
-print('# Querying from {} {{#{}}}\n'.format(c.FULLNAMES[query_city],
-                                            query_city))
-nogt = pd.DataFrame(data={"City": map(lambda x: c.FULLNAMES[x[0]], NO_GOLD),
-                          "District": map(itemgetter(1), NO_GOLD)})
-if len(nogt) > 0:
-    print('There is no ground truth for:\n')
-    print(nogt.to_html(index=False))
-RES_QUERIES = {(c, d) for c in results.keys() for d in results[c].keys()}
-NO_RES = sorted(ALL_QUERIES.difference(set(NO_GOLD), RES_QUERIES))
-nogt = pd.DataFrame(data={"City": map(lambda x: c.FULLNAMES[x[0]], NO_RES),
-                          "District": map(itemgetter(1), NO_RES)})
-if len(nogt) > 0:
-    print('\nNo result for:\n')
-    print(nogt.to_html(index=False))
-RES_QUERIES.difference_update(NO_GOLD)
+if __name__ == '__main__':
+    RES_SIZE = 5
+    query_city = sys.argv[1]
+    with open('static/cmp_{}.json'.format(query_city)) as gt:
+        results = json.load(gt)
+    METRICS = sorted(set([str(_['metric'])
+                          for _ in results.values()[0].values()[0]]))
+    METRICS = ['femd']
+    with open('static/ground_truth.json') as gt:
+        regions = json.load(gt)
+    DISTRICTS = sorted(regions.keys())
+    CITIES = sorted(regions.items()[0][1]['gold'].keys())
+    assert query_city in CITIES, ', '.join(CITIES)
+    CITIES.remove(query_city)
+    ALL_QUERIES = set(itertools.product(CITIES, DISTRICTS))
+    NO_GOLD = []
+    for d, g in regions.iteritems():
+        g = g['gold']
+        missing = set(CITIES).difference(set(g.keys()))
+        for city in missing:
+            NO_GOLD.append((city, d))
+    print_region = lambda x: '  • '+str(x[0]).ljust(15)+str(x[1])
+    print('# Querying from {} {{#{}}}\n'.format(c.FULLNAMES[query_city],
+                                                query_city))
+    nogt = pd.DataFrame(data={"City": map(lambda x: c.FULLNAMES[x[0]], NO_GOLD),
+                              "District": map(itemgetter(1), NO_GOLD)})
+    # if len(nogt) > 0:
+    #     print('There is no ground truth for:\n')
+    #     print(nogt.to_html(index=False))
+    RES_QUERIES = {(c, d) for c in results.keys() for d in results[c].keys()}
+    # NO_RES = sorted(ALL_QUERIES.difference(set(NO_GOLD), RES_QUERIES))
+    # nogt = pd.DataFrame(data={"City": map(lambda x: c.FULLNAMES[x[0]], NO_RES),
+    #                           "District": map(itemgetter(1), NO_RES)})
+    # if len(nogt) > 0:
+    #     print('\nNo result for:\n')
+    #     print(nogt.to_html(index=False))
+    RES_QUERIES.difference_update(NO_GOLD)
 
 
 def jaccard(s1, s2):
@@ -62,8 +64,8 @@ def jaccard(s1, s2):
 def relevance(one_result, all_gold):
     return max([jaccard(one_result, g) for g in all_gold])
 
-
-discount_factor = np.log2(np.arange(1, RES_SIZE+1) + 1)
+if __name__ == '__main__':
+    discount_factor = np.log2(np.arange(1, RES_SIZE+1) + 1)
 
 
 def pad_list(short_list, new_size, value=0.0):
@@ -75,8 +77,8 @@ def DCG(relevance):
     relevance = pad_list(relevance, RES_SIZE)
     return np.sum((np.array(relevance)**2 - 1) / discount_factor)
 
-
-LOWEST = -DCG(RES_SIZE*[0.0, ])
+if __name__ == '__main__':
+    LOWEST = -DCG(RES_SIZE*[0.0, ])
 
 
 def compute_scores(raw_result):
@@ -109,65 +111,68 @@ def final_result(raw_results):
                           for metric, dcgs in scores.iteritems()],
                          key=lambda x: -x[1])
     return final_order, scores, distances
-ww, scores, distances = final_result(results)
-nogt = pd.DataFrame(data={"Metric": map(itemgetter(0), ww),
-                          "Score": map(itemgetter(1), ww)})
-print('\n## Overall score\n')
-print(nogt.to_html(index=False))
-
-# Plot of individual query:
-# QUERIES = [city[:3]+'_'+district[:5]
-#            for city, district in itertools.product(CITIES, DISTRICTS)
-#            if (city, district) not in MISSING]
-# lines = []
-# plt.xticks(range(len(QUERIES)), QUERIES, rotation=45)
-# for idx, dcgs in enumerate(scores.itervalues()):
-#     lines.extend(ppl.plot(dcgs, lw=1.25, c=ppl.colors.set1[idx]))
-# _=plt.legend(lines, scores.keys())
-# _=plt.xlabel('query')
-# _=plt.ylabel('score')
-
-# There is no obvious correlation between the average raw distance of each
-# result and the score of the corresponding metric
-# for idx, metric in enumerate(METRICS):
-#     # ppl.plot(scores[metric], np.mean(distances[metric], 1), '.', ms=6,
-#     #          c=ppl.colors.set1[idx])
-#     _ = ppl.scatter(scores[metric], np.mean(distances[metric], 1))
-# _ = plt.xlabel('score')
-# _ = plt.ylabel('mean distance')
-
-# We can also break the scores by cities
-import pandas as pd
-mpl.rcParams['figure.figsize'] = (10, 6.5)
-city_mask = np.array([CITIES.index(city) for city, _ in RES_QUERIES])
 
 
-def score_by_cities(scores, order):
-    per_city = {metric: [np.mean(np.array(scores[metric])[city_mask == cidx])
-                         for cidx in range(len(CITIES))]
-                for metric in METRICS}
-    city_names = map(c.FULLNAMES.__getitem__, CITIES)
-    mat = pd.DataFrame(index=city_names, data=per_city)
-    mat = mat[[_[0] for _ in order]]
-    ordered_index = np.array(city_names)[np.argsort(mat.mean(1).values)[::-1]]
-    return mat.reindex(pd.Index(ordered_index), copy=False)
+if __name__ == '__main___':
+    ww, scores, distances = final_result(results)
+    nogt = pd.DataFrame(data={"Metric": map(itemgetter(0), ww),
+                              "Score": map(itemgetter(1), ww)})
+    print('\n## Overall score\n')
+    print(nogt.to_html(index=False))
+
+    # Plot of individual query:
+    # QUERIES = [city[:3]+'_'+district[:5]
+    #            for city, district in itertools.product(CITIES, DISTRICTS)
+    #            if (city, district) not in MISSING]
+    # lines = []
+    # plt.xticks(range(len(QUERIES)), QUERIES, rotation=45)
+    # for idx, dcgs in enumerate(scores.itervalues()):
+    #     lines.extend(ppl.plot(dcgs, lw=1.25, c=ppl.colors.set1[idx]))
+    # _=plt.legend(lines, scores.keys())
+    # _=plt.xlabel('query')
+    # _=plt.ylabel('score')
+
+    # There is no obvious correlation between the average raw distance of each
+    # result and the score of the corresponding metric
+    # for idx, metric in enumerate(METRICS):
+    #     # ppl.plot(scores[metric], np.mean(distances[metric], 1), '.', ms=6,
+    #     #          c=ppl.colors.set1[idx])
+    #     _ = ppl.scatter(scores[metric], np.mean(distances[metric], 1))
+    # _ = plt.xlabel('score')
+    # _ = plt.ylabel('mean distance')
+
+    # We can also break the scores by cities
+    import pandas as pd
+    mpl.rcParams['figure.figsize'] = (10, 6.5)
+    city_mask = np.array([CITIES.index(city) for city, _ in RES_QUERIES])
 
 
-def plot_score(mat):
-    ppl.pcolormesh(np.flipud(mat.values), xticklabels=mat.columns.tolist(),
-                   yticklabels=mat.index.tolist()[::-1],
-                   # , norm=mpl.colors.LogNorm(vmin=mat.values.min(),
-                   #                           vmax=mat.values.max())
-                   cmap=mpl.cm.YlOrBr)
+    def score_by_cities(scores, order):
+        per_city = {metric: [np.mean(np.array(scores[metric])[city_mask == cidx])
+                             for cidx in range(len(CITIES))]
+                    for metric in METRICS}
+        city_names = map(c.FULLNAMES.__getitem__, CITIES)
+        mat = pd.DataFrame(index=city_names, data=per_city)
+        mat = mat[[_[0] for _ in order]]
+        ordered_index = np.array(city_names)[np.argsort(mat.mean(1).values)[::-1]]
+        return mat.reindex(pd.Index(ordered_index), copy=False)
 
 
-mat = score_by_cities(scores, ww)
-print('## Score by city')
-print(mat.to_html())
-plot_score(mat)
-plt.title('Score in {} with no venues weight'.format(c.FULLNAMES[query_city]))
-img_name = 'metrics_from_{}.png'.format(query_city)
-plt.savefig(img_name, dpi=96, transparent=False, frameon=False,
-            bbox_inches='tight', pad_inches=0.1)
-plt.clf()
-print('<img src="{}">\n\n'.format(img_name))
+    def plot_score(mat):
+        ppl.pcolormesh(np.flipud(mat.values), xticklabels=mat.columns.tolist(),
+                       yticklabels=mat.index.tolist()[::-1],
+                       # , norm=mpl.colors.LogNorm(vmin=mat.values.min(),
+                       #                           vmax=mat.values.max())
+                       cmap=mpl.cm.YlOrBr)
+
+
+    mat = score_by_cities(scores, ww)
+    print('## Score by city')
+    print(mat.to_html())
+    plot_score(mat)
+    plt.title('Score in {} with no venues weight'.format(c.FULLNAMES[query_city]))
+    img_name = 'metrics_from_{}.png'.format(query_city)
+    plt.savefig(img_name, dpi=96, transparent=False, frameon=False,
+                bbox_inches='tight', pad_inches=0.1)
+    plt.clf()
+    print('<img src="{}">\n\n'.format(img_name))

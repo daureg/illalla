27966b336ce9abfe4c70cab21ff03211740be6f6
Géraud Le Falher <daureg@gmail.com>
2014-08-26 00:03:21 +0200

    display result from exhaustive and approximate search on top of gold

--- static/gold.js
+++ static/gold.js
@@ -1,9 +1,21 @@
+// http://stackoverflow.com/a/2880929
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+    pl     = /\+/g,  // Regex for replacing addition symbol with a space
+    search = /([^&=]+)=?([^&]*)/g,
+    decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+    query  = window.location.search.substring(1);
+    urlParams = {};
+    while (match = search.exec(query))
+        urlParams[decode(match[1])] = decode(match[2]);
+})();
 var VENUES_LOC = {};
 var GOLD = new L.FeatureGroup();
 var LEFT_CANVAS = null, LC_VISIBLE = false;
 var map = create_map('map', BBOX, {zoomAnimation: false});
 var left = map;
-populate('left', canvas_display);
+// populate('left', canvas_display);
 var MyLayer = L.FullCanvas.extend({
     drawSource: function(point, ctx) {
         ctx.beginPath();
@@ -27,8 +39,8 @@ function canvas_display(result, nside, map) {
 	LC_VISIBLE = true;
     map.addLayer(venue_dots);
 }
-function geojson_to_polygon(geo) {
-	var style = {color: '#b22222', opacity: 0.6};
+function geojson_to_polygon(geo, pstyle) {
+	var style = pstyle || {color: '#b22222', opacity: 0.6};
 	if (geo.type === 'Polygon') {
 		var coords = geo.coordinates[0], latlngs = [];
 		for (var i = 0, l = coords.length; i < l-1; i++) {
@@ -36,33 +48,57 @@ function geojson_to_polygon(geo) {
 		}
 		return L.polygon(latlngs, style);
 	}
-	return L.circle([geo.center[1], geo.center[0]], geo.radius, style);
+	return L.circle([geo.center[0], geo.center[1]], geo.radius, style);
 }
 function plot_gold(features) {
 	var poly = null,
 		bounds = null;
 	GOLD.clearLayers();
 	for (var i = 0, l = features.length; i < l; i++) {
-		console.log(features[i].geometry);
 		poly = geojson_to_polygon(features[i].geometry);
 		if (bounds) {bounds.extend(poly.getBounds());}
 		else {bounds = poly.getBounds();}
 		GOLD.addLayer(poly);
 	}
-    map.fitBounds(bounds, {maxZoom: 14});
+    map.fitBounds(bounds, {maxZoom: 12});
+}
+function plot_answers(features, metric) {
+	var poly = null;
+	for (var i = 0, l = features.length; i < l; i++) {
+		if (features[i].metric === metric && features[i].pos < 4) {
+			console.log(features[i].geo);
+			poly = geojson_to_polygon(features[i].geo, {color: '#5677fc', opacity: 0.6});
+			// console.log(poly);
+			GOLD.addLayer(poly);
+		}
+	}
 }
 var AREAS = null;
 $(function() {
+	console.log(urlParams);
 	map.addLayer(GOLD);
     $.request('get', $SCRIPT_ROOT+'/static/ground_truth.json')
 	.then(function get_gt(result) {
-		console.log('get something');
 		AREAS = $.parseJSON(result);
 		plot_gold(AREAS[DISTRICT].gold[CITY]);
 	})
     .error(function(status, statusText, responseText) {
         console.log(status, statusText, responseText);
     });
+	if (urlParams.m === 'emd') {
+		$.request('get', $SCRIPT_ROOT+'/static/cmp_'+urlParams.from+'.json')
+		.then(function get_emd(result) {
+			AREAS = $.parseJSON(result);
+			plot_answers(AREAS[CITY][DISTRICT], 'emd');
+		});
+	}
+	if (urlParams.m === 'femd') {
+		$.request('get', $SCRIPT_ROOT+'/static/'+urlParams.from+'_'+DISTRICT+'_'+CITY+'.json')
+		.then(function get_emd(result) {
+			AREAS = $.parseJSON(result);
+			plot_answers(AREAS, 'femd');
+		});
+	}
 // {65: 'A', 66: 'B', 67: 'C', 68: 'D', 69: 'E', 70: 'F', 71: 'G', 72: 'H',
 // 73: 'I', 74: 'J', 75: 'K', 76: 'L', 77: 'M', 78: 'N', 79: 'O', 80: 'P', 81:
 // 'Q', 82: 'R', 83: 'S', 84: 'T', 85: 'U', 86: 'V', 87: 'W', 88: 'X', 89:
